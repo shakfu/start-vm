30f56f72437a101626186478cfb7135ec4b300b4
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2021-07-24 21:34:29 +0300

    fixed patchbox

--- config/patchbox/awesome/debian/menu.lua
+++ config/patchbox/awesome/debian/menu.lua
@@ -1,32 +1,33 @@
 -- automatically generated file. Do not edit (see /usr/share/doc/menu/html)
 
-module("debian.menu")
+local awesome = awesome
 
 Debian_menu = {}
 
-Debian_menu["Debian_Applications_Accessibility"] = {
-	{"Xmag","xmag"},
-}
 Debian_menu["Debian_Applications_Editors"] = {
 	{"scite","/usr/bin/scite","/usr/share/scite/Sci32M.xpm"},
-	{"Xedit","xedit"},
 }
 Debian_menu["Debian_Applications_Graphics"] = {
-	{"X Window Snapshot","xwd | xwud"},
-}
-Debian_menu["Debian_Applications_Network_Communication"] = {
-	{"Telnet", "x-terminal-emulator -e ".."/usr/bin/telnet.netkit"},
-	{"Xbiff","xbiff"},
+	{"dotty","/usr/bin/dotty"},
+	{"GEM","/usr/bin/pd-gem","/usr/share/pixmaps/gem.xpm"},
+	{"lefty","/usr/bin/lefty"},
 }
 Debian_menu["Debian_Applications_Network_Web_Browsing"] = {
 	{"w3m", "x-terminal-emulator -e ".."/usr/bin/w3m /usr/share/doc/w3m/MANUAL.html"},
 }
 Debian_menu["Debian_Applications_Network"] = {
-	{ "Communication", Debian_menu["Debian_Applications_Network_Communication"] },
 	{ "Web Browsing", Debian_menu["Debian_Applications_Network_Web_Browsing"] },
 }
+Debian_menu["Debian_Applications_Programming"] = {
+	{"Erlang Shell", "x-terminal-emulator -e ".."/usr/bin/erl","/usr/share/pixmaps/erlang.xpm"},
+	{"GDB", "x-terminal-emulator -e ".."/usr/bin/gdb"},
+	{"Tclsh8.6", "x-terminal-emulator -e ".."/usr/bin/tclsh8.6"},
+	{"TkWish8.6","x-terminal-emulator -e /usr/bin/wish8.6"},
+}
 Debian_menu["Debian_Applications_Science_Mathematics"] = {
-	{"Xcalc","xcalc"},
+	{"Bc", "x-terminal-emulator -e ".."/usr/bin/bc"},
+	{"Dc", "x-terminal-emulator -e ".."/usr/bin/dc"},
+	{"galculator","/usr/bin/galculator","/usr/share/pixmaps/galculator.xpm"},
 }
 Debian_menu["Debian_Applications_Science"] = {
 	{ "Mathematics", Debian_menu["Debian_Applications_Science_Mathematics"] },
@@ -36,13 +37,12 @@ Debian_menu["Debian_Applications_Shells"] = {
 	{"Dash", "x-terminal-emulator -e ".."/bin/dash -i"},
 	{"Sh", "x-terminal-emulator -e ".."/bin/sh --login"},
 }
+Debian_menu["Debian_Applications_Sound"] = {
+	{"Cecilia","/usr/bin/cecilia"},
+}
 Debian_menu["Debian_Applications_System_Administration"] = {
 	{"Debian Task selector", "x-terminal-emulator -e ".."su-to-root -c tasksel"},
 	{"Editres","editres"},
-	{"Orphaner (all)", "x-terminal-emulator -e ".."su-to-root -c '/usr/sbin/orphaner -a'"},
-	{"Orphaner - editkeep", "x-terminal-emulator -e ".."su-to-root -c '/usr/sbin/editkeep'"},
-	{"Orphaner (libs)", "x-terminal-emulator -e ".."su-to-root -c /usr/sbin/orphaner"},
-	{"Xclipboard","xclipboard"},
 	{"Xfontsel","xfontsel"},
 	{"Xkill","xkill"},
 	{"Xrefresh","xrefresh"},
@@ -53,44 +53,56 @@ Debian_menu["Debian_Applications_System_Hardware"] = {
 Debian_menu["Debian_Applications_System_Monitoring"] = {
 	{"Pstree", "x-terminal-emulator -e ".."/usr/bin/pstree.x11","/usr/share/pixmaps/pstree16.xpm"},
 	{"Top", "x-terminal-emulator -e ".."/usr/bin/top"},
-	{"Xconsole","xconsole -file /dev/xconsole"},
 	{"Xev","x-terminal-emulator -e xev"},
-	{"Xload","xload"},
 }
 Debian_menu["Debian_Applications_System"] = {
 	{ "Administration", Debian_menu["Debian_Applications_System_Administration"] },
 	{ "Hardware", Debian_menu["Debian_Applications_System_Hardware"] },
 	{ "Monitoring", Debian_menu["Debian_Applications_System_Monitoring"] },
 }
-Debian_menu["Debian_Applications_Viewers"] = {
-	{"Xditview","xditview"},
-}
 Debian_menu["Debian_Applications"] = {
-	{ "Accessibility", Debian_menu["Debian_Applications_Accessibility"] },
 	{ "Editors", Debian_menu["Debian_Applications_Editors"] },
 	{ "Graphics", Debian_menu["Debian_Applications_Graphics"] },
 	{ "Network", Debian_menu["Debian_Applications_Network"] },
+	{ "Programming", Debian_menu["Debian_Applications_Programming"] },
 	{ "Science", Debian_menu["Debian_Applications_Science"] },
 	{ "Shells", Debian_menu["Debian_Applications_Shells"] },
+	{ "Sound", Debian_menu["Debian_Applications_Sound"] },
 	{ "System", Debian_menu["Debian_Applications_System"] },
-	{ "Viewers", Debian_menu["Debian_Applications_Viewers"] },
-}
-Debian_menu["Debian_Games_Toys"] = {
-	{"Oclock","oclock"},
-	{"Xclock (analog)","xclock -analog"},
-	{"Xclock (digital)","xclock -digital -update 1"},
-	{"Xeyes","xeyes"},
-	{"Xlogo","xlogo"},
-}
-Debian_menu["Debian_Games"] = {
-	{ "Toys", Debian_menu["Debian_Games_Toys"] },
 }
 Debian_menu["Debian_Help"] = {
 	{"Info", "x-terminal-emulator -e ".."info"},
-	{"Xman","xman"},
+}
+Debian_menu["Debian_Programming"] = {
+	{"Sonic Pi","/usr/bin/sonic-pi","/usr/share/pixmaps/sonic-pi.png"},
+}
+Debian_menu["Debian_Screen_Locking"] = {
+	{"Lock Screen (XScreenSaver)","/usr/bin/xscreensaver-command -lock"},
+}
+Debian_menu["Debian_Screen_Saving"] = {
+	{"Activate ScreenSaver (Next)","/usr/bin/xscreensaver-command -next"},
+	{"Activate ScreenSaver (Previous)","/usr/bin/xscreensaver-command -prev"},
+	{"Activate ScreenSaver (Random)","/usr/bin/xscreensaver-command -activate"},
+	{"Demo Screen Hacks","/usr/bin/xscreensaver-command -demo"},
+	{"Disable XScreenSaver","/usr/bin/xscreensaver-command -exit"},
+	{"Enable XScreenSaver","/usr/bin/xscreensaver"},
+	{"Reinitialize XScreenSaver","/usr/bin/xscreensaver-command -restart"},
+	{"ScreenSaver Preferences","/usr/bin/xscreensaver-command -prefs"},
+}
+Debian_menu["Debian_Screen"] = {
+	{ "Locking", Debian_menu["Debian_Screen_Locking"] },
+	{ "Saving", Debian_menu["Debian_Screen_Saving"] },
+}
+Debian_menu["Debian_Window_Managers"] = {
+	{"awesome",function () awesome.exec("/usr/bin/awesome") end,"/usr/share/pixmaps/awesome.xpm"},
 }
 Debian_menu["Debian"] = {
 	{ "Applications", Debian_menu["Debian_Applications"] },
-	{ "Games", Debian_menu["Debian_Games"] },
 	{ "Help", Debian_menu["Debian_Help"] },
+	{ "Programming", Debian_menu["Debian_Programming"] },
+	{ "Screen", Debian_menu["Debian_Screen"] },
+	{ "Window Managers", Debian_menu["Debian_Window_Managers"] },
 }
+
+debian = { menu = { Debian_menu = Debian_menu } }
+return debian
